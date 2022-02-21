279af30b609437a8aedfb5d1371945adc809c69e
Jin <qjsro368@gmail.com>
2022-02-21 20:48:49 +0900

    t

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as HashRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/index';
 import SigninPage from './pages/signin'
 
 function App() {
   return (
     <>
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path = '/' component = {Home} exact={true}/>
         <Route path = '/signin' component = {SigninPage} exact={true}/>
       </Switch>
-    </HashRouter>
+    </Router>
     </>
   );
 }
